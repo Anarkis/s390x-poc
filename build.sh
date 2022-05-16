0b6a81933cdf6d82ac672cdf0cd9a157d06253fb
Luis Tovar <luis.tovar@suse.coM>
2022-05-16 16:54:06 +0200

    Added push command

--- build.sh
+++ build.sh
@@ -4,3 +4,12 @@ set -x
 
 echo "Executing $0"
 echo "SUCCESS!!!"
+
+cat secrets.txt | docker login --username luistt --password-stdin
+docker pull ubuntu
+docker tag ubuntu luistt/s390x-poc:latest
+docker push luistt/s390x-poc:latest
+
+dockery system prune --all -f
+
+echo "Finish pushing"
